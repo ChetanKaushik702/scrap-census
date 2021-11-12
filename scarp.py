6be632471d6db7e955345c0d6bdb59293df255c7
Chetan Kaushik <kaushikchetan199@gmail.com>
2021-11-12 13:50:08 +0530

    Scraped Haryana census home page to receive a list of towns/villages in the mentioned districts

---
+++ scarp.py
@@ -0,0 +1,22 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+
+source = requests.get('http://www.citypopulation.de/en/india/haryana/').text
+soup = BeautifulSoup(source, 'lxml')
+rows = soup.find('table', id='ts').tbody.find_all('tr')
+dict = {}
+for row in rows:
+    city = row.find('td', class_='rname').a.text
+    district = row.find('td', class_='radm rarea').text
+    if district in dict and district != city:
+        val = dict[district]
+        val.append(city)
+        dict[district] = val
+    elif district != city:
+        val = [city]
+        dict[district] = val
+
+# with open('towns.json', 'w') as outfile:
+#     json.dump(dict, outfile)
+print(len(dict.keys()))
